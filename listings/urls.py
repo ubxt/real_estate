2f5aed82cb91ca8cb661e559b3e039bc9727037a
ubxt <ubtofficial@hotmail.com>
2023-01-17 00:18:38 +0300

    Implemented crud for listing

--- listings/urls.py
+++ listings/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import listing_list
+from .views import *
 
 urlpatterns = [
     path("", listing_list, name="listing_list"),
+    path("<pk>/", listing_retrieve, name="listing_retrieve"),
+    path("create", listing_create, name="listing_create"),
+    path("update/<pk>/", listing_update, name="listing_update"),
+    path("delete/<pk>/", listing_delete, name="listing_delete"),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Listing
+from .forms import ListingForm
 
 
 def listing_list(request):
     queryset = Listing.objects.all()
     context = {"listings": queryset}
     return render(request=request, template_name="listings.html", context=context)
+
+
+def listing_retrieve(request, pk):
+    listing = Listing.objects.get(pk=pk)
+    context = {"listing": listing}
+    return render(request, "listing.html", context)
+
+
+def listing_create(request):
+    if request.method == "POST":
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/listings")
+    else:
+        form = ListingForm()
+
+    context = {"form": form}
+    return render(request, "listing_create.html", context)
+
+
+def listing_update(request, pk):
+    listing = Listing.objects.get(pk=pk)
+    if request.method == "POST":
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            form.save()
+            return redirect("/listings")
+    else:
+        form = ListingForm(instance=listing)
+
+    context = {"form": form}
+    return render(request, "listing_update.html", context)
+
+
+def listing_delete(request, pk):
+    listing = Listing.objects.get(pk=pk)
+    listing.delete()
+    return redirect("/listings")
